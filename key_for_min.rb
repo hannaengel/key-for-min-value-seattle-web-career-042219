6a85b5779d0d749f467cde122f21a6002fdbc14d
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-17 00:03:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  smallest = 1000000
+  actual = 'placeholder'
+  name_hash.each do |name, value|
+    if value < smallest==true
+      smallest = value
+      actual = name
+    end
+  end
+  actual
 end
