8396ce33272adc0d6172eaeb3dbce9cb961c6456
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-17 00:04:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 def key_for_min_value(name_hash)
   smallest = 1000000
-  actual = 'placeholder'
+  actual = 'nil'
   name_hash.each do |name, value|
     if value < smallest==true
       smallest = value
